358ddeaf75c4a208460563df03e05e97c10e26e5
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-10 18:33:06 +1000

    Update test_subnet_visualizer.py

--- tests/test_subnet_visualizer.py
+++ tests/test_subnet_visualizer.py
@@ -1,1 +1,11 @@
 
+from src.subnet_visualizer import summarize_subnet
+
+
+
+
+def test_summarize_subnet():
+info = summarize_subnet('192.168.1.10', '/28')
+assert info['prefixlen'] == 28
+assert info['num_addresses'] == 16
+assert info['network'] == '192.168.1.0'
