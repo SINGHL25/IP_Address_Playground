545aa42933d62ac865555d96fc776daa52d3e95f
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-10 18:33:24 +1000

    Update test_ip_validator.py

--- tests/test_ip_validator.py
+++ tests/test_ip_validator.py
@@ -1,1 +1,16 @@
 
+from src.ip_validator import validate_ipv4, validate_ipv6
+
+
+
+
+def test_validate_ipv4():
+assert validate_ipv4('192.168.1.1')
+assert not validate_ipv4('300.1.1.1')
+
+
+
+
+def test_validate_ipv6():
+assert validate_ipv6('2001:0db8::1')
+assert not validate_ipv6('not_an_ip')
